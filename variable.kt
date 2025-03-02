a9bc059984dc689f49fcf2b159453ee231be380a
chamdarig <109315064+chamdarig@users.noreply.github.com>
2025-03-03 00:28:10 +0100

    Update variable.kt

--- variable.kt
+++ variable.kt
@@ -8,5 +8,15 @@ fun main() {
     val count: Int = 2
     println("You have $count unread messages.")
 }
+
+Note: If you don't provide an initial value when you declare a variable, you must specify the type.
+
+In this line of code, no initial value is provided, so you must specify the data type:
+
+val count: Int
+
+In this line of code, an assigned value is provided, so you can omit the data type:
+
+val count = 2
 */
 val count = 2

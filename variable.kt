a6680b45988f9cf28ffbca4f6efb5c71e078279b
chamdarig <109315064+chamdarig@users.noreply.github.com>
2025-03-03 00:35:31 +0100

    Update variable.kt

--- variable.kt
+++ variable.kt
@@ -29,11 +29,17 @@ fun main() {
     val readCount = 100
     println("You have ${unreadCount + readCount} total messages in your inbox.")
 }
-*/
+
 fun main() {
     val numberOfPhotos = 100
     val photosDeleted = 10
     println("$numberOfPhotos photos")
     println("$photosDeleted photos deleted")
     println("${numberOfPhotos - photosDeleted} photos left")
+}
+*/
+fun main() {
+    val cartTotal = 0
+    cartTotal = 20
+    println("Total: $cartTotal")
 }
